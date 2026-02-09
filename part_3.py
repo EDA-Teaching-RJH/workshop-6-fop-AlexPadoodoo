e8ee826b813baebbb3907ecf885ec03a193974a3
AlexPadoodoo <app10@kent.ac.uk>
2026-02-09 12:28:57 +0000

    first draft of task 3.1

--- part_3.py
+++ part_3.py
@@ -1,1 +1,8 @@
+#Task 3.1: THe motor controller
+print("----- Motor Speed is between 0 and 100 mph -----")
 
+try:
+    motor_speed = int(input("Enter Motor Speed: "))
+    print(f"Speed Set To: {motor_speed}", "mph")
+except ValueError:
+    print("Error: Corrupted Signal. Maintaining Current Speed")
