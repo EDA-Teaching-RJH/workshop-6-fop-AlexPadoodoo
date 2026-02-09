287c1e40fcdc8f510925bdcd8367016a2038719b
AlexPadoodoo <app10@kent.ac.uk>
2026-02-09 12:18:24 +0000

    did 2.2 status update

--- part_2.py
+++ part_2.py
@@ -1,8 +1,17 @@
 #Task 2.1: System Status
 rover_status = {
     "Battery": 100,
-    "Heater": "off",
+    "Heater": "Off",
     "Camera": "Standby"
 }
 
 print(f"Battery Charge: {rover_status["Battery"]}","%")
+
+
+#Task 2.2: System Update
+#Car has now turned on
+rover_status.update({"Battery": 85})
+rover_status.update({"Heater": "On"})
+rover_status.update({"Speed": 5})
+
+print(rover_status)
