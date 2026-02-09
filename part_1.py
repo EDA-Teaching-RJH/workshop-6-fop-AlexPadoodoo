68aeba5d734e915570caea2c35a4011c35a4cb11
AlexPadoodoo <app10@kent.ac.uk>
2026-02-09 11:31:23 +0000

    attempting task 1.3

--- part_1.py
+++ part_1.py
@@ -1,6 +1,16 @@
-#Variable containing inital strings
+#Task 1.1: Variable containing inital strings
 sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
 
-#Analyzing Samples with iterations
+#Task 1.2: Analyzing Samples with iterations
 for sample in sample_bay:
     print(f"Transmitting Data for: {sample}")
+
+
+#Task 1.3: Creating a new list 
+
+new_findings = []
+
+i = 3
+while i !=0:
+    rock_type = str(input("Enter a rock type: "))
+    i-1
