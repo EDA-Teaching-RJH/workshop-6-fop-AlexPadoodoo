a3481cf98573e0ee34d07929c93476d97ea6cbfb
AlexPadoodoo <app10@kent.ac.uk>
2026-02-09 11:45:29 +0000

    Completed task 1.3

--- part_1.py
+++ part_1.py
@@ -7,11 +7,13 @@ for sample in sample_bay:
 
 
 #Task 1.3: Creating a new list 
-
 new_findings = []
 
 i = 3
 while i:
     i = i - 1
     rock_type = str(input("Enter a rock type: "))
+    new_findings.append(f"{rock_type}")
 
+for findings in new_findings:
+    print(f"New rock type findings: {findings}")
